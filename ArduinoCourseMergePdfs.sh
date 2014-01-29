74132287dc23f5de5eaf640c16f4b36bf67b22b8
Richel Bilderbeek <richelbilderbeek@gmail.com>
2014-01-29 16:25:13 +0100

    Initial commit

---
+++ ArduinoCourseMergePdfs.sh
@@ -0,0 +1,19 @@
+# Convert all LyX files to PDF
+lyx -e pdf2 `find . | egrep "\.lyx"`
+
+# Concatenate all PDFs in the correct order
+pdfunite \
+  ArduinoCourseIntroduction.pdf \
+  ArduinoCourseMindset.pdf \
+  ArduinoCourseWhatIsArduino.pdf \
+  ArduinoCourseConnectLed.pdf \
+  ArduinoCourseAll.pdf
+
+#Convert ArduinoCourseAll.pdf to handout format (4 columns, 2 rows)
+java -cp Multivalent.jar tool.pdf.Impose -dim 4x2 -paper 84x29.7cm ArduinoCourseAll.pdf
+
+#Rename the resulting PDF
+mv ArduinoCourseAll-up.pdf ArduinoCourseAllHandouts.pdf
+
+#Negate the resulting PDF
+convert ArduinoCourseAllHandouts.pdf -negate ArduinoCourseAllHandoutsNegated.pdf
