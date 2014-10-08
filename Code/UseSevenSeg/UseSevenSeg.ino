daa5585878e942a34b42bdede835736875f9ca0b
remer <remernl@users.noreply.github.com>
2014-10-08 14:55:53 +0200

    Update UseSevenSeg.ino

--- Code/UseSevenSeg/UseSevenSeg.ino
+++ Code/UseSevenSeg/UseSevenSeg.ino
@@ -3,6 +3,20 @@
 // Initializes 7 seg display SevenSeg(int pinSegA, int pinSegB, int pinSegC, int pinSegD, int pinSegE, int pinSegF, int pinSegG, int pinDot, boolean isCathode)
 SevenSeg MySevenSeg = SevenSeg(1,2,3,4,5,6,7,8, true); // Enter correct pin numbers. Default: isCathode = true
 
+void countFromTo(const int firstDigit, const int lastDigit, const int delayTime = 1000) {
+  if (firstDigit < lastDigit) {
+    for (int n = firstDigit; n <= lastDigit; ++n) { // count forwards
+      MySevenSeg.writeDigit(n);
+      delay(delayTime);
+    }  
+  } else {
+      for (int n = firstDigit; n > lastDigit; --n) { // count backwards
+        MySevenSeg.writeDigit(n - 1);
+        delay(delayTime);
+      }
+  }
+}
+
 void setup() {}
 
 void loop() {
@@ -12,6 +26,6 @@ void loop() {
   MySevenSeg.writeDigit(8);
   MySevenSeg.clearDisplay();
   
-  MySevenSeg.countFromTo(9,2,500); // Usage: (firstDigit, lastDigit, delayTime). Default: delayTime = 1000 (ms) 
+  countFromTo(9,2,500); // Usage: (firstDigit, lastDigit, delayTime). Default: delayTime = 1000 (ms) 
   MySevenSeg.clearDisplay(3000); // Usage: (delayTime). Default: delayTime = 1000 (ms) 
 }
