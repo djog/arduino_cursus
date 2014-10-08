31e6eaa3a907f3b3d4966eea4597f7ce2c0bd6d2
remer <remernl@users.noreply.github.com>
2014-10-08 17:36:50 +0200

    Update UseSevenSeg.ino

--- Code/UseSevenSeg/UseSevenSeg.ino
+++ Code/UseSevenSeg/UseSevenSeg.ino
@@ -3,16 +3,16 @@
 // Initializes 7 seg display SevenSeg(int pinSegA, int pinSegB, int pinSegC, int pinSegD, int pinSegE, int pinSegF, int pinSegG, int pinDot, boolean isCathode)
 SevenSeg MySevenSeg = SevenSeg(1,2,3,4,5,6,7,8, true); // Enter correct pin numbers. Default: isCathode = true
 
-void countFromTo(const int firstDigit, const int lastDigit, const int delayTime = 1000) {
+void countFromTo(const int firstDigit, const int lastDigit, const int delayInMS = 1000) {
   if (firstDigit < lastDigit) {
     for (int n = firstDigit; n <= lastDigit; ++n) { // count forwards
       MySevenSeg.writeDigit(n);
-      delay(delayTime);
+      delay(delayInMS);
     }  
   } else {
       for (int n = firstDigit; n > lastDigit; --n) { // count backwards
         MySevenSeg.writeDigit(n - 1);
-        delay(delayTime);
+        delay(delayInMS);
       }
   }
 }
@@ -24,7 +24,7 @@ void loop() {
   MySevenSeg.clearDisplay(); 
   
   MySevenSeg.writeDigit(8);
-  MySevenSeg.clearDisplay(1000); // Usage: (delayTime). Default: delayTime = 0 (ms) 
+  MySevenSeg.clearDisplay(1000); // Usage: (duration). Default: duration = 0 (ms) 
 
   // Calculation example
   int a = 2;
@@ -34,7 +34,7 @@ void loop() {
   MySevenSeg.writeDigit(a+b); 
   
   // Counter example
-  countFromTo(9,2,500); // Usage: (firstDigit, lastDigit, delayTime). Default: delayTime = 1000 (ms) 
+  countFromTo(9,2,500); // Usage: (firstDigit, lastDigit, delayInMS). Default: delayInMS = 1000 (ms) 
   MySevenSeg.clearDisplay(3000);
   
   // Stopwatch example
