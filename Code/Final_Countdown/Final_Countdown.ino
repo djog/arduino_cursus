f7b14ca68a23dac8cbed16a127ad62c532eb9f55
richelbilderbeek <richel@richelbilderbeek.nl>
2015-01-24 07:45:49 +0100

    Improved countdown timer

--- Code/Final_Countdown/Final_Countdown.ino
+++ Code/Final_Countdown/Final_Countdown.ino
@@ -1,6 +1,8 @@
 // Countdown timer
 //
 // (C) 2015 Arlette Reneman & Winand Slingenbergh
+// with the help of Paul van Boven
+// again, adapted by Richel Bilderbeek
 //
 // Released under the GPLv3 license
 //
@@ -9,29 +11,36 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN            6
+const int pin_leds = 6;
+
+// Which pin on the Arduino is connected to the piezo?
+const int pin_piezo = 10;
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      59
+const int n_pixels = 59;
 
-const unsigned long factor = (5 *60 * (unsigned long)1000) / 59;
+const unsigned long factor = (5 * 60 * (unsigned long)1000) / 59;
 const unsigned long factor2 = (60 * 5 * (unsigned long)1000) / 255;
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(n_pixels, pin_leds, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pixels.begin();
+  pinMode(pin_piezo,OUTPUT);
 }
 
 
 void loop() {
-
   const int ledpower = millis() / factor2;
   const int n_lampjes = millis() / factor;
   pixels.setPixelColor(n_lampjes, pixels.Color(ledpower , 255 - ledpower, 0));
   pixels.show(); 
-  delay(10);
+  if (n_lampjes >= 59)
+  {
+    tone(pin_piezo,440,1000);    
+  }
+  delay(100);
 }
