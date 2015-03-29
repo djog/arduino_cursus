4fc8566e7ef562afb2b256a971b0b715bd4e77f6
richelbilderbeek <richel@richelbilderbeek.nl>
2015-03-29 19:51:38 +0200

    Moved ArduinoTrialShield to ArduinoTrialShield GitHub

--- Code/ReadTimeFromSerial/ReadTimeFromSerial.ino
+++ Code/ReadTimeFromSerial/ReadTimeFromSerial.ino
@@ -5,13 +5,17 @@ void setup()
 
 void loop() 
 {
+  if (!Serial.available()) { delay(100); return; }
   const int h = Serial.available() ? Serial.parseInt() : 0;
-  const char c1 = Serial.available() ? Serial.read() : ',';
+  const char c1 = Serial.available() ? Serial.read() : ':';
   const int m = Serial.available() ? Serial.parseInt() : 0;
-  Serial.print(h);
+  const char c2 = Serial.available() ? Serial.read() : ':';
+  const int s = Serial.available() ? Serial.parseInt() : 0;
+  Serial.print(h*2);
   Serial.print(c1);
-  Serial.println(m);
-  delay(1000);
+  Serial.print(m*2);
+  Serial.print(c2);
+  Serial.println(s*2);
 }
 
 
