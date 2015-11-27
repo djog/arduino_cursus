1f951437f4e6652caa3da5cc9a3b39d71b6e06b1
jongeonderzoekers <jongeonderzoekers@x2011>
2015-11-27 19:07:13 +0100

    Added evaluatie

---
+++ Dingen/Evaluatie/Evaluatie.ino
@@ -0,0 +1,34 @@
+Evaluatie Dojo
+
+Op een Arduino avond vind ik de sfeer
+( ) super leuk     
+( ) leuk
+( ) gaat wel       
+( ) niet leuk
+
+De uitleg vind ik meestal
+( ) goed te snappen           
+( ) met moeite te snappen
+( ) niet te snappen
+
+Ik vind dat ik
+( ) veel geleerd heb
+( ) een beetje geleerd
+( ) weinig geleerd heb
+
+Ik vind dat de leraren dit erg goed doen:
+
+
+
+
+Ik vind dat de leraren dit meer moeten doen:
+
+
+
+
+Als ik iets aan de cursus mocht veranderen, dan zou ik:
+
+
+
+
+Als ik de cursus een cijfer zou moeten geven, dan geef ik een:
