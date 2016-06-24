e20a5f3a5ad0a9f7ebbdb644af4f891a8fa0388a
richelbilderbeek <richel@richelbilderbeek.nl>
2016-06-24 14:34:21 +0200

    Evaluatie

---
+++ Evaluatie/Evaluatie.ino
@@ -0,0 +1,43 @@
+/* Arduino cursus evaluatie
+
+Ik kom vooral bij de Arduino cursus om iets te leren 
+[ ] mee eens
+[ ] geen mening
+[ ] niet mee eens
+
+Ik zou willen dat we serieuzer werkten 
+[ ] mee eens
+[ ] geen mening
+[ ] niet mee eens
+
+Op een Arduino avond vind ik de sfeer
+[ ] gezellig
+[ ] gaat wel
+[ ] vervelend
+
+Ik vind dat ik dit seizoen
+[ ] veel geleerd heb
+[ ] een beetje geleerd
+[ ] weinig geleerd heb
+
+Ik vind dat Anko ...
+
+Ik vind dat Jan ...
+
+Ik vind dat Peter ...
+
+Ik vind dat Richel ...
+
+Ik vind dat Sara ...
+
+Ik vind goed aan de cursus, dat ...
+
+
+
+Als ik iets aan de cursus mocht veranderen, dan zou ik ...
+
+
+
+
+Ik geef de cursus het cijfer (van 1 tot en met 10):
+*/
