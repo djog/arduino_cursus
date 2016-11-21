08ddd76256de4fbf5f1d831a151b80f38271f7d1
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-21 21:50:38 +0100

    Flyer

--- Dingen/Presentaties/Presentaties.ino
+++ Dingen/Presentaties/Presentaties.ino
@@ -1,32 +1,23 @@
-/*
--------------------------------------------------------------------------
+/*-----------------------------------------------------------------------
                             PRESENTATIE ARDUINO CURSUS 
 -------------------------------------------------------------------------
-
-Wie: leerlingen van de Arduino cursus
- * Nano's ( 7-12 jaar): 18:00-20:15
- * Uno's  (12-18 jaar): 19:45-21:30
-Wanneer: 1 juli 2016
-Hoe laat: 19:00-21:00
+Wanneer: vrijdag 16 december 2016
 Waar: De Jonge Onderzoekers, Dirk Huizingastraat 13
 -------------------------------------------------------------------------
                   PLANNING (dit zijn natuurlijk richtlijnen!)
 -------------------------------------------------------------------------
-Nano's      | Uno's       | 
-18:00-18:15 | 19:15-19:45 | Inloop
-18:15-18:45 | 19:45-20:15 | (1) Voorbereiding && inleiding
-18:45-19:15 | 20:15-20:45 | Presentaties
-19:15-19:30 | 20:45-21:00 | Pauze
-19:45-20:10 | 21:00-21:25 | (2) Evaluatie
-20:10-20:15 | 21:25-21:30 | Afsluiting
-
-(1) Voorbereiding && inleiding
+18:30-19:00 Inloop
+19:00-19:30 Voorbereiding && inleiding
  * Leerlingen: @ houtlokaal om presentaties klaar te zetten
  * Ouders: @ kantine, waar Arduino cursus leraren vertellen 
            wat de leerlingen hebben gedaan
-(2) Evaluatie
+19:30-20:30 Presentaties
+20:30-20:45 Pauze
+20:45-21:00 Evaluatie
  * Leerlingen: @ houtlokaal met Mr/Mrs X 
  * Ouders: @ kantine met leraren
+21:00 KLAAR!
+
 -------------------------------------------------------------------------
                                 FAQ
 -------------------------------------------------------------------------
