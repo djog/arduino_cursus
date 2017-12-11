326e12273cd310f8fa6f82da7070dc8b43783c94
antonhensen <antonhensen@hotmail.com>
2017-12-11 13:03:54 +0100

    Tim zijn code
    Code die Tim wil laten zien bij presentaties

---
+++ Leerlingen/koel.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo mijn_servo;
+
+void setup()
+{
+  Serial.begin(9600);
+  mijn_servo.attach(9);
+  pinMode(A0, INPUT);
+}
+
+void loop()
+{
+  mijn_servo.write(
+    map(analogRead(A0),0,1024,30,160)
+    );
+    delay(100);
+}
+
