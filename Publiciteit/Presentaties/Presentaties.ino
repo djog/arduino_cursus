6fb1b181c383d78a053636ae0bb4b69c0a3a9d12
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-21 22:01:24 +0100

    EOD

--- Publiciteit/Presentaties/Presentaties.ino
+++ Publiciteit/Presentaties/Presentaties.ino
@@ -1,40 +1,25 @@
-void setup() 
-{
-  /*-----------------------------------------------------------------------
-                              PRESENTATIE ARDUINO CURSUS 
-  -------------------------------------------------------------------------
-  Wanneer: vrijdag 15 december 2017
-  Waar: De Jonge Onderzoekers, Dirk Huizingastraat 13
-  -------------------------------------------------------------------------
-                    PLANNING (dit zijn natuurlijk richtlijnen!)
-  -------------------------------------------------------------------------
-  18:00-18:15 Inloop
-  18:15-18:45 Voorbereiding && inleiding
-   * Deelnemers: @ houtlokaal, code klaarzetten
-   * Ouders: @ kantine, prelude door vrijwilligers
-  18:45-19:15 Presentaties
-  19:15-19:30 Pauze
-  19:30-20:00 Evaluatie
-   * Deelnemers: @ houtlokaal met Mr/Mrs X 
-   * Ouders: @ kantine met begeleiders
-  20:00 KLAAR!
-  
-  -------------------------------------------------------------------------
-                                  FAQ
-  -------------------------------------------------------------------------
-  Q: Mag ik mensen (familie, vrienden, enz.) meenemen?
-  A: Natuurlijk!
-  
-  Q: Mijn machine/programma is nog niet af! Is dat erg?
-  A: Nee hoor. Je laat gewoon zien wat je wel hebt gedaan.
-  
-  */
-} 
+--------------------------------------------------------
+                 PRESENTATIE ARDUINO CURSUS 
+--------------------------------------------------------
+Wanneer: vrijdag 14 december 2018
+Waar: De Jonge Onderzoekers, Dirk Huizingastraat 13
+--------------------------------------------------------
+         PLANNING (dit zijn natuurlijk richtlijnen!)
+--------------------------------------------------------
+18:00-18:15 Inloop
+18:15-19:00 Open Les door leerlingen voor ouders
+19:00-19:15 Pauze
+19:15-19:30 Presentaties Uno's (13+)
+19:30-20:00 Evaluatie
+ * Deelnemers: @ houtlokaal met Mr/Mrs X 
+ * Ouders: @ kantine met begeleiders
+20:00 KLAAR!
 
-void loop()
-{
- 
- 
-  
-}
+--------------------------------------------------------
+                                FAQ
+--------------------------------------------------------
+Q: Mag ik mensen (familie, vrienden, enz.) meenemen?
+A: Nou en of! Leuk!
 
+Q: Mijn machine/programma is nog niet af! Is dat erg?
+A: Nee hoor. Je laat gewoon zien wat je wel hebt gedaan.
